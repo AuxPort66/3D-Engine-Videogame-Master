9725904d70946599d9bda8942e82ce61c876ad95
Sara Izquierdo <93783740+AuxPort66@users.noreply.github.com>
2022-12-08 16:46:44 +0100

    [rearrenge] assimp lib

---
+++ Source/assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Sara/Desktop/Master/C++ Jueves/Engine-3D/Engine/Source/assimp/contrib/zlib
+# Build directory: C:/Users/Sara/Desktop/Master/C++ Jueves/Engine-3D/Engine/Source/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
