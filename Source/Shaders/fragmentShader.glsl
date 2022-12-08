25cf0e620450b6faa55ec7c1d26de1bc6a8ea5ee
Sara Izquierdo <93783740+AuxPort66@users.noreply.github.com>
2022-12-08 18:12:17 +0100

    [feat] [Texture] Shaders Update for Texture

--- Source/Shaders/fragmentShader.glsl
+++ Source/Shaders/fragmentShader.glsl
@@ -1,7 +1,8 @@
 #version 330 core
-out vec4 color;
-
+out vec4 FragColor;
+in vec2 uv0;
+uniform sampler2D mytexture;
 void main()
 {
-	color = vec4(1.0, 0.0, 0.0, 1.0);
+	FragColor = texture2D(mytexture, uv0);
 };
