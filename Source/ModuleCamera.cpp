246cf6c469cb441824be3039374fdde8e9a9d7ff
Sara Izquierdo <93783740+AuxPort66@users.noreply.github.com>
2022-12-08 17:44:00 +0100

    [feat] [Camera] Key Movement Camera

--- Source/ModuleCamera.cpp
+++ Source/ModuleCamera.cpp
@@ -38,6 +38,8 @@ bool ModuleCamera::Init()
 }
 
 update_status ModuleCamera::Update() {
+
+	MovementCameraKey();
 	return UPDATE_CONTINUE;
 }
 
@@ -54,4 +56,87 @@ float4x4 ModuleCamera::GetProjectionMatrix()
 float4x4 ModuleCamera::GetViewMatrix()
 {
 	return frustum.ViewMatrix();
+}
+
+void ModuleCamera::SetPosition(float3 pos) {
+	frustum.SetPos(pos);
+}
+
+void ModuleCamera::SetFOV(float newhorizontalFov)
+{
+	horizontalFov = newhorizontalFov;
+	frustum.SetHorizontalFovAndAspectRatio(newhorizontalFov, aspect);
+}
+
+void ModuleCamera::SetAspectRatio(float newAspect)
+{
+	aspect = newAspect;
+	frustum.SetHorizontalFovAndAspectRatio(frustum.HorizontalFov(), newAspect);
+}
+
+void ModuleCamera::AddPosition(float3 pos)
+{
+	frustum.SetPos(frustum.Pos() + pos);
+}
+
+void ModuleCamera::RotateCamera(float3 direction, float angleRadiants) {
+	float3x3 rotationDeltaMatrix;
+	rotationDeltaMatrix = rotationDeltaMatrix.RotateAxisAngle(direction, angleRadiants);
+
+	vec oldFront = frustum.Front().Normalized();
+	frustum.SetFront(rotationDeltaMatrix.MulDir(oldFront));
+	vec oldUp = frustum.Up().Normalized();
+	frustum.SetUp(rotationDeltaMatrix.MulDir(oldUp));
+}
+
+void ModuleCamera::LookAt(float3& point)
+{
+	float3 look_direction = point - frustum.Pos();
+
+	float3x3 matrix = float3x3::LookAt(frustum.Front(), look_direction.Normalized(), frustum.Up(), float3::unitY);
+
+	frustum.SetFront(matrix.MulDir(frustum.Front()).Normalized());
+	frustum.SetUp(matrix.MulDir(frustum.Up()).Normalized());
+}
+
+void ModuleCamera::MovementCameraKey()
+{
+	float speedMov = 0.001f;
+	float speedRot = 0.02f;
+
+	if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) {
+		AddPosition(float3(frustum.Front() * speedMov));
+	}
+	if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) {
+		AddPosition(float3(-frustum.Front() * speedMov));
+	}
+	if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
+		AddPosition(float3(frustum.WorldRight() * -speedMov));
+	}
+	if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
+		AddPosition(float3(frustum.WorldRight() * speedMov));
+	}
+	if (App->input->GetKey(SDL_SCANCODE_E) == KEY_REPEAT) {
+		AddPosition(float3(0.0f, -speedMov, 0.0f));
+	}
+	if (App->input->GetKey(SDL_SCANCODE_Q) == KEY_REPEAT) {
+		AddPosition(float3(0.0f, speedMov, 0.0f));
+	}
+	if (App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT) {
+		RotateCamera(frustum.WorldRight(), speedRot * (pi / 180));
+	}
+	if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT) {
+		RotateCamera(-frustum.WorldRight(), speedRot * (pi / 180));
+	}
+	if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT) {
+		RotateCamera(-float3::unitY, speedRot * (pi / 180));
+	}
+	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT) {
+		RotateCamera(float3::unitY, speedRot * (pi / 180));
+	}	
+	
+	if (App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT || App->input->GetKey(SDL_SCANCODE_RSHIFT) == KEY_REPEAT) {
+		speedMov += speedMov; speedRot += speedRot;
+	}
+
 }
--- Source/ModuleCamera.h
+++ Source/ModuleCamera.h
@@ -16,6 +16,15 @@ public:
     float4x4 GetProjectionMatrix();
     float4x4 GetViewMatrix();
 
+    void SetFOV(float newhorizontalFov);
+    void SetAspectRatio(float newAspect);
+    void SetPosition(float3 pos);
+    void AddPosition(float3 pos);
+    void LookAt(float3& point);
+    void MovementCameraKey();
+    void RotateCamera(float3 direction, float angleRadiants);
+
+
 private:
     Frustum frustum;
     float nearPlaneDistance;
